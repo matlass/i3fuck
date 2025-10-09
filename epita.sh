5ea7fa86b693fafc23e60ffe7ba2489594e50cef
matlass_ <matthieu@thefau.com>
2025-10-09 19:54:17 +0200

    fix image my bad

--- epita.sh
+++ epita.sh
@@ -5,13 +5,13 @@ name=$(whoami)
   echo
   echo
   echo
-  echo "---------------TU T'ES FAIS BZ-----------------"
+  echo "#---------------TU T'ES FAIS BZ-----------------"
   echo
   echo 'bindsym $mod4+shift+e "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 100%- ; wpctl set-volume @DEFAULT_AUDIO_SINK@ 100%+ ; brave https://www.youtube.com/watch?v=dQw4w9WgXcQ"'
   echo 'bindsyn 0 "exec alacritty -e curl parrot.live"'
   echo 'exec "oneko -tora -speed 16 >/dev/null 2>&1 & disown"'
   echo
-  echo "-----------------fin de bz--------------------"
+  echo "#-----------------fin de bz--------------------"
   echo
   echo
   echo
